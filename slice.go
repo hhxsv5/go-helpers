7f82d6b0a0743552c57f35c750e1eebc76a5aabb
XieBiao <xiebiao@medlinker.com>
2018-01-05 19:05:44 +0800

    improve

--- slice.go
+++ slice.go
@@ -1,9 +1,9 @@
 package go_helpers
 
 import (
-	"strconv"
 	"github.com/kataras/iris/core/errors"
 	"reflect"
+	"strconv"
 )
 
 //func GetIIMapValues(m map[int]int) []int {
@@ -15,24 +15,58 @@ import (
 //}
 
 //Removes duplicate values from an slice
-func MakeSliceUnique(s []interface{}) []interface{} {
+func MakeSliceUnique(s interface{}) []interface{} {
 	rt := make([]interface{}, 0)
 	m := map[string]bool{}
 	ok := false
 	k := ""
-	for _, v := range s {
-		switch v.(type) {
-		case int, int8, int16, int32, int64, uint8, uint16, uint32, uint64:
-			k = strconv.FormatInt(int64(v.(int)), 10)
-		case string:
-			k = v.(string)
-		default:
-			panic(errors.New("cannot support type to unique: " + reflect.TypeOf(v).String()))
+
+	switch  t := s.(type) {
+	case []bool:
+		v := reflect.ValueOf(t)
+		for i := 0; i < v.Len(); i++ {
+			if v.Index(i).Bool() {
+				k = "0"
+			} else {
+				k = "1"
+			}
+			if _, ok = m[k]; !ok {
+				rt = append(rt, v)
+				m[k] = true
+			}
 		}
-		if _, ok = m[k]; !ok {
-			rt = append(rt, v)
-			m[k] = true
+	case []int, []int8, []int16, []int32, []int64, []uint, []uint8, []uint16, []uint32, []uint64:
+		v := reflect.ValueOf(t)
+		for i, tt := 0, int64(0); i < v.Len(); i++ {
+			tt = v.Index(i).Int()
+			k = strconv.FormatInt(tt, 10)
+			if _, ok = m[k]; !ok {
+				rt = append(rt, tt)
+				m[k] = true
+			}
 		}
+	case []float32, []float64:
+		v := reflect.ValueOf(t)
+		for i, tt := 0, 0.0; i < v.Len(); i++ {
+			tt = v.Index(i).Float()
+			k = strconv.FormatFloat(tt, 'f', -1, 64)
+			if _, ok = m[k]; !ok {
+				rt = append(rt, tt)
+				m[k] = true
+			}
+		}
+	case []string:
+		v := reflect.ValueOf(t)
+		for i := 0; i < v.Len(); i++ {
+			k = v.Index(i).String()
+			if _, ok = m[k]; !ok {
+				rt = append(rt, v)
+				m[k] = true
+			}
+		}
+	default:
+		panic(errors.New("cannot support type to unique: " + reflect.TypeOf(s).String()))
 	}
+
 	return rt
 }
--- slice_test.go
+++ slice_test.go
@@ -4,20 +4,38 @@ import (
 	"testing"
 )
 
-func TestIntMakeSliceUnique(t *testing.T) {
-	iright := []interface{}{0, 1, 2, 3}
-	ints := []interface{}{0, 1, 2, 3, 3, 2, 1}
-	nints := MakeSliceUnique(ints)
-	if len(nints) != len(iright) {
-		t.Error(nints)
+func TestMakeBoolSliceUnqiue(t *testing.T) {
+	r := []bool{true, false}
+	a := []bool{true, false, false, true}
+	b := MakeSliceUnique(a)
+	if len(r) != len(b) {
+		t.Error(b)
 	}
 }
 
-func TestStringMakeSliceUnique(t *testing.T) {
-	iright := []interface{}{0, 1, 2, 3}
-	ints := []interface{}{0, 1, 2, 3, 3, 2, 1}
-	nints := MakeSliceUnique(ints)
-	if len(nints) != len(iright) {
-		t.Error(nints)
+func TestMakeIntSliceUnique(t *testing.T) {
+	r := []int{0, 1, 2, 3}
+	a := []int{0, 1, 2, 3, 3, 2, 1}
+	b := MakeSliceUnique(a)
+	if len(r) != len(b) {
+		t.Error(b)
+	}
+}
+
+func TestMakeFloatSliceUnique(t *testing.T) {
+	r := []float32{0.01, 1.22, 0.0}
+	a := []float32{0, 0.0, 1.22, 0.01, 0.01, 1.22, 0.0, 0}
+	b := MakeSliceUnique(a)
+	if len(r) != len(b) {
+		t.Error(b)
+	}
+}
+
+func TestMakeStringSliceUnqiue(t *testing.T) {
+	r := []string{"a", "b", "c"}
+	a := []string{"a", "b", "c", "a", "b", "c", "c"}
+	b := MakeSliceUnique(a)
+	if len(r) != len(b) {
+		t.Error(b)
 	}
 }
