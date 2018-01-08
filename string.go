9bab2ec927ad5f46ee5b11b634cdc19ccab0bfc7
XieBiao <xiebiao@medlinker.com>
2018-01-08 20:29:33 +0800

    str convert

--- string.go
+++ string.go
@@ -1,1 +1,21 @@
 package go_helpers
+
+import (
+	"strconv"
+)
+
+func StrToInt64(s string) int64 {
+	r, e := strconv.ParseInt(s, 10, 64)
+	if e != nil {
+		return 0
+	}
+	return r
+}
+
+func StrToFloat64(s string) float64 {
+	r, e := strconv.ParseFloat(s, 64)
+	if e != nil {
+		return 0
+	}
+	return r
+}
--- string_test.go
+++ string_test.go
@@ -1,1 +1,20 @@
 package go_helpers
+
+import "testing"
+
+func TestStrToInt64(t *testing.T) {
+	i := StrToInt64("3000")
+	if i != 3000 {
+		t.Error(i)
+	}
+	t.Log(i)
+
+}
+
+func TestStrToFloat64(t *testing.T) {
+	i := StrToFloat64("3000.1234")
+	if i != 3000.1234 {
+		t.Error(i)
+	}
+	t.Log(i)
+}
